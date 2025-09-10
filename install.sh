fbfdf7aa140c6e724991b69b8929522bf7c37e37
Guillaume CABANEL <guillaume@kuartz.fr>
2025-09-10 21:58:21 +0200

    fix chmod

--- install.sh
+++ install.sh
@@ -5,17 +5,16 @@ set -e
 echo "Install packages"
 sudo apt-get install -y $(cat ~/.local/share/debready/install/packages_list) > /dev/null
 
-echo "Check if we're in a D-Bus session, if not, start one"
 if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
     eval $(dbus-launch --sh-syntax)
     export DBUS_SESSION_BUS_ADDRESS
 fi
 
-echo -e "\nAdd boot splash"
+echo "Add boot splash"
 ~/.local/share/debready/install/plymouth.sh
 
-echo -e "\nGnome settings"
+echo "Gnome settings"
 ~/.local/share/debready/install/gnome_settings.sh
 
-echo -e "\nGnome shortcuts"
+echo "Gnome shortcuts"
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/.local/share/debready/install/shortcuts.ini

8e605af244c159d116c4fe5274f41c4b15c2aca3
Guillaume CABANEL <guillaume@kuartz.fr>
2025-09-10 21:56:25 +0200

    boot fix

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-# Install packages
-sudo apt-get install -y $(cat ~/.local/share/debready/install/packages_list)
+echo "Install packages"
+sudo apt-get install -y $(cat ~/.local/share/debready/install/packages_list) > /dev/null
 
 echo "Check if we're in a D-Bus session, if not, start one"
 if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
@@ -12,10 +12,10 @@ if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
 fi
 
 echo -e "\nAdd boot splash"
-./install/plymouth.sh
+~/.local/share/debready/install/plymouth.sh
 
 echo -e "\nGnome settings"
-./install/gnome_settings.sh
+~/.local/share/debready/install/gnome_settings.sh
 
 echo -e "\nGnome shortcuts"
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/.local/share/debready/install/shortcuts.ini

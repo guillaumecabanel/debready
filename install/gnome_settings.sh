f34ff9473c2ded467f6e328b0439879a28fa4296
Guillaume CABANEL <guillaume@kuartz.fr>
2025-09-10 21:18:39 +0200

    fix dconf path

--- install/gnome_settings.sh
+++ install/gnome_settings.sh
@@ -10,20 +10,20 @@ AutomaticLogin=$CURRENT_USER
 AutomaticLoginEnable=true" | sudo tee /etc/gdm3/custom.conf > /dev/null
 
 # Compose key => capslock
-sudo dconf write "org.gnome.desktop.input-sources/xkb-options" "['compose:caps']"
+sudo dconf write "/org/gnome/desktop.input-sources/xkb-options" "['compose:caps']"
 
 # Power button behaviour => nothing
-sudo dconf write "org.gnome.settings-daemon.plugins.power/power-button-action" "'nothing'"
+sudo dconf write "/org/gnome/settings-daemon/plugins/power/power-button-action" "'nothing'"
 
 # Show battery percentage => ON
-sudo dconf write "org.gnome.desktop.interface/show-battery-percentage" "true"
+sudo dconf write "/org/gnome/desktop/interface/show-battery-percentage" "true"
 
 # Automatic screen blank => OFF
-sudo dconf write "org.gnome.desktop.session/idle-delay" "uint32 0"
+sudo dconf write "/org/gnome/desktop/session/idle-delay" "uint32 0"
 
 # Automatic suspend on battery power => ON, 20 minutes
-sudo dconf write "org.gnome.settings-daemon.plugins.power/sleep-inactive-battery-type" "'suspend'"
-sudo dconf write "org.gnome.settings-daemon.plugins.power/sleep-inactive-battery-timeout" "1200"
+sudo dconf write "/org/gnome/settings-daemon/plugins/power/sleep-inactive-battery-type" "'suspend'"
+sudo dconf write "/org/gnome/settings-daemon/plugins/power/sleep-inactive-battery-timeout" "1200"
 
 # Automatic suspend when plugged in => OFF
-sudo dconf write "org.gnome.settings-daemon.plugins.power/sleep-inactive-ac-type" "'nothing'"
+sudo dconf write "/org/gnome/settings-daemon/plugins/power/sleep-inactive-ac-type" "'nothing'"

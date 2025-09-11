01b99cfb26dc7412e501c1cb40c20994433ae097
Guillaume CABANEL <guillaume@kuartz.fr>
2025-09-11 13:32:54 +0200

    fix shortcut + ohmyzsh

--- install.sh
+++ install.sh
@@ -10,11 +10,14 @@ if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
     export DBUS_SESSION_BUS_ADDRESS
 fi
 
-echo "Add boot splash"
-~/.local/share/debready/install/plymouth.sh
-
 echo "Gnome settings"
 ~/.local/share/debready/install/gnome_settings.sh
 
 echo "Gnome shortcuts"
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/.local/share/debready/install/shortcuts.ini
+dconf load "/org/gnome/settings-daemon/plugins/media-keys/" < ~/.local/share/debready/install/shortcuts.ini
+
+echo "Terminal"
+~/.local/share/debready/install/terminal.sh
+
+echo "Add boot splash"
+~/.local/share/debready/install/plymouth.sh
---
+++ install/terminal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Oh My Zsh…"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

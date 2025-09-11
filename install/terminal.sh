e1c36cae729a2e4f776b1a1d4432afcea394f273
Guillaume CABANEL <guillaume@kuartz.fr>
2025-09-11 14:01:13 +0200

    fix dotfile

--- install/terminal.sh
+++ install/terminal.sh
@@ -5,4 +5,6 @@ set -e
 echo "Installing Oh My Zsh…"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+cd ~/.local/share/debready/dotfiles
 stow -v -t "$HOME" alacritty
+cd -

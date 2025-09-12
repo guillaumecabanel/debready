158cf9b35aaf131fca0bf46daf9363902d1d8680
Guillaume CABANEL <guillaume@kuartz.fr>
2025-09-12 20:48:48 +0200

    font

--- install/font.sh
+++ install/font.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-wget -qO- https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
+wget -qO- https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip > CascadiaMono.zip
 mkdir -p ~/.local/share/fonts
 unzip -o CascadiaMono.zip -d ~/.local/share/fonts
 rm CascadiaMono.zip

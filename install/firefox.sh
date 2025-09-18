a43272ca0347fa1f272ee9b00c30af2a67542c20
Guillaume CABANEL <guillaume@kuartz.fr>
2025-09-18 17:12:59 +0200

    fix firefox

--- install/firefox.sh
+++ install/firefox.sh
@@ -4,6 +4,7 @@ set -e
 
 sudo install -d -m 0755 /etc/apt/keyrings
 mkdir ~/.gnupg
+chmod 700 ~/.gnupg
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
 
@@ -12,7 +13,7 @@ Types: deb
 URIs: https://packages.mozilla.org/apt
 Suites: mozilla
 Components: main
-Signed-By: /etc/apt/keyrings/packages.mozilla.org.asc" | sudo tee /etc/apt/sources.list.d/mozilla.list > /dev/null
+Signed-By: /etc/apt/keyrings/packages.mozilla.org.asc" | sudo tee /etc/apt/sources.list.d/mozilla.sources > /dev/null
 
 echo "
 Package: *

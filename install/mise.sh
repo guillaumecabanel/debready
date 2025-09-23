67d36a08b24d57032e5dea1109906bea5b0cc3ef
Guillaume CABANEL <guillaume@kuartz.fr>
2025-09-23 18:22:33 +0200

    setup mise

---
+++ install/mise.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+
+echo "
+Types: deb
+URIs: https://mise.jdx.dev/deb
+Suites: stable
+Components: main
+Signed-By: /etc/apt/keyrings/mise-archive-keyring.gpg" | sudo tee /etc/apt/sources.list.d/mise.sources > /dev/null
+
+
+sudo apt update
+sudo apt install -y mise
---
+++ install/rails.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+mise install ruby
+gem install -N bundler rails mailcatcher

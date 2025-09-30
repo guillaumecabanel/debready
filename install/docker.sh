38482e1f99a3dfaf4a842fa5e24a68d7857e4711
Guillaume CABANEL <guillaume@kuartz.fr>
2025-09-30 22:20:48 +0200

    setup docker

---
+++ install/docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+wget -q https://download.docker.com/linux/debian/gpg -O- | sudo tee /etc/apt/keyrings/docker.asc >/dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: trixie
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc" | sudo tee /etc/apt/sources.list.d/docker.sources >/dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker ${USER}
+
+# Limit log size to avoid running out of disk
+echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json

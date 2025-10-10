84f5f6ea26ae1dd185d3abb1c7e0ce22cc2d47d2
Guillaume CABANEL <guillaume@kuartz.fr>
2025-10-10 16:40:48 +0200

    clone i3 for i3

--- boot.sh
+++ boot.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y git > /dev/null
 
 echo "Cloning Debready…"
 rm -rf ~/.local/share/debready
-git clone https://github.com/guillaumecabanel/debready.git ~/.local/share/debready > /dev/null
+git clone -b i3 https://github.com/guillaumecabanel/debready.git ~/.local/share/debready > /dev/null
 
 echo "Preparing your Debian…"
 source ~/.local/share/debready/install.sh

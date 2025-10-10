18d09d2801c86c96fa97e53ce8d967c31c7db837
Guillaume CABANEL <guillaume@kuartz.fr>
2025-10-10 14:06:06 +0100

    i3

--- install.sh
+++ install.sh
@@ -24,31 +24,14 @@ for i in "${!PACKAGES[@]}"; do
 done
 echo ""
 
-if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-  eval $(dbus-launch --sh-syntax)
-  export DBUS_SESSION_BUS_ADDRESS
-fi
-
 echo "Installing Nerd Font…"
 ~/.local/share/debready/install/font.sh
-
-echo "Setting up Gnome…"
-~/.local/share/debready/install/gnome_settings.sh
-
-echo "Setting up shortcuts…"
-dconf load "/org/gnome/settings-daemon/plugins/media-keys/" <~/.local/share/debready/install/shortcuts.ini
 
 echo "Setting up terminal…"
 ~/.local/share/debready/install/terminal.sh
 
-echo "Installing Chrome…"
-~/.local/share/debready/install/chrome.sh
-
 echo "Installing Firefox…"
 ~/.local/share/debready/install/firefox.sh
-
-echo "Installing Cursor…"
-~/.local/share/debready/install/cursor.sh
 
 echo "Installing Mise…"
 ~/.local/share/debready/install/mise.sh
@@ -59,21 +42,5 @@ echo "Installing Rails…"
 echo "Installing Docker…"
 ~/.local/share/debready/install/docker.sh
 
-echo "Making things beautiful…"
-cd ~/.local/share/debready/dotfiles
-stow -t "$HOME" theme-switcher
-cd -
-systemctl --user enable theme-switcher.service
-systemctl --user start theme-switcher.service
 ~/.local/share/debready/install/plymouth.sh
 
-echo "Schedule post reboot script on first terminal start"
-cat >~/.config/autostart/alacritty.desktop <<EOF
-[Desktop Entry]
-Type=Application
-Exec=/usr/bin/alacritty -e $HOME/.local/share/debready/post_reboot.sh
-Hidden=false
-X-GNOME-Autostart-enabled=true
-Name=Post Reboot
-Comment=Run post reboot script
-EOF
